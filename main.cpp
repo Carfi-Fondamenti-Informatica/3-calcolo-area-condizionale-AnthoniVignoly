d63ff455371d2302f17d3c96becff818e84c6058
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-11-18 10:52:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+    int a;
+    int b;
+    int area=0;
+
+    cin >> area >> a >> b;
+   
+       switch(area){
+        case 0:
+            cout << ((a*b)/2) ;
+            break;
+        case 1:
+            cout << (a*a);
+            break;
+        case 2:
+            cout << (a*b);
+            break;
+        default:
+            cout << "Opzione non valida";
+            break;
+    }
+
    return 0;
 }
